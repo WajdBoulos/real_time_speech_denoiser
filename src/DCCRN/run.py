675635566140cede4168d2ed862ca1caec7e60df
Wajd <wajd.boulos@campus.technion.ac.il>
2021-12-23 21:02:14 +0000

    adjust batch size and dropband

--- src/DCCRN/run.py
+++ src/DCCRN/run.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     sample_rate = 16000
     preprocess(data_dir, json_dir, sample_rate)
 
-    batch_size = 4
+    batch_size = 12
     max_hours = None  # only use some of the data for tests. should be None when running on full dataset
     num_workers = 4
     train(data_dir, epochs, batch_size, model_path, model_features_path, max_hours=max_hours,
--- src/DCCRN/solver.py
+++ src/DCCRN/solver.py
@@ -270,10 +270,13 @@ class Solver(object):
 
         noisy_mag, _ = mag_phase(noisy_complex)
         ground_truth_cIRM = build_complex_ideal_ratio_mask(noisy_complex, clean_complex)  # [B, F, T, 2]
-        ground_truth_cIRM = drop_band(
-            ground_truth_cIRM.permute(0, 3, 1, 2),  # [B, 2, F ,T]
-            self.model.module.num_groups_in_drop_band
-        ).permute(0, 2, 3, 1)
+        if not cross_valid:
+            ground_truth_cIRM = drop_band(
+                ground_truth_cIRM.permute(0, 3, 1, 2),  # [B, 2, F ,T]
+                self.model.module.num_groups_in_drop_band
+            ).permute(0, 2, 3, 1)
+        else:
+            ground_truth_cIRM = build_complex_ideal_ratio_mask(noisy_complex, clean_complex)  # [B, F, T, 2]
 
         with autocast(enabled=False):
             # [B, F, T] => [B, 1, F, T] => model => [B, 2, F, T] => [B, F, T, 2]
